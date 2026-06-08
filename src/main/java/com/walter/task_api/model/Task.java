f2329bbea2f510d9c47b4cb9e4e75664c63ec85c
Walter <josuewalterespejoguarachi@gmail.com>
2026-06-08 09:52:09 -0400

    Start project

---
+++ src/main/java/com/walter/task_api/model/Task.java
@@ -0,0 +1,55 @@
+package com.walter.task_api.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    private String description;
+
+    private boolean completed = false;
+
+    // Getters y setters (o usa Lombok: @Data en la clase)
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
